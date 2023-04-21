ca35707faa7a5e73a1f811c484c034e481204335
anita04021 <131482579+anita04021@users.noreply.github.com>
2023-04-21 20:08:07 +0200

    Update interaction.py

--- interaction.py
+++ interaction.py
@@ -10,3 +10,8 @@ st.write("My First Web App")
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.write(chart_data)
 
+image = Image.open('/Users/anitabaculima/Desktop/myapp/logo1.jpg')
+st.image(image)
+st.dataframe(chart_data)
+option = st.selectbox('Select',['a','b','c'])
+st.bar_chart(chart_data[option])
